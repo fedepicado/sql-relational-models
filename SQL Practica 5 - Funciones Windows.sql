b739c8bad8a1c613696eef631325361b90193f52
Federico <fedepicado@gmail.com>
2025-05-23 23:43:51 -0300

    Avances practica 5

--- SQL Practica 5 - Funciones Windows.sql
+++ SQL Practica 5 - Funciones Windows.sql
@@ -43,30 +43,125 @@ FROM Sales.SalesOrderHeader
 -- 3. Promedio acumulado de salarios.
 -- Para cada empleado, calcula el promedio acumulado (AVG() OVER) de salarios ordenado por la fecha de ingreso (HireDate).
 
-SELECT TOP 5 * FROM Sales.SalesOrderDetail
+SELECT TOP 5 * FROM HumanResources.EmployeePayHistory
+SELECT TOP 5 * FROM Person.Person
+SELECT TOP 5 * FROM HumanResources.EmployeeDepartmentHistory
+SELECT TOP 5 * FROM HumanResources.Employee
 
-SELECT TOP 5 * FROM Sales.SalesOrderHeader
-
+SELECT 
+	p.FirstName,
+	p.LastName,
+	AVG(eph.rate) OVER (
+	PARTITION BY eph.BusinessEntityID
+	) AS Promedio_empleado,
+	eph.rate AS rate -- para chequear nomas
+FROM HumanResources.EmployeePayHistory eph
+INNER JOIN Person.Person p
+	ON p.BusinessEntityID=eph.BusinessEntityID
+INNER JOIN HumanResources.EmployeeDepartmentHistory edh
+	ON edh.BusinessEntityID= eph.BusinessEntityID
+INNER JOIN HumanResources.Employee e
+	ON e.BusinessEntityID = eph.BusinessEntityID
+ORDER BY e.HireDate DESC;
 
 
 -- 4. Ventas acumuladas por cliente.
 --Para cada cliente, muestra cada orden junto con el total acumulado de compras (SubTotal) ordenadas por fecha.
 
+SELECT TOP 5* FROM Sales.SalesOrderHeader
+
+SELECT 
+	CustomerID AS Cliente,
+	SalesOrderID AS ID_venta,
+	OrderDate AS Fecha_orden,
+	SUM(SubTotal) OVER (
+	PARTITION BY CustomerID
+	ORDER BY OrderDate
+	) AS Total_Acumulado
+FROM Sales.SalesOrderHeader
+
+
 -- 5. Diferencia de salario respecto al anterior.
 -- Para cada empleado ordenado por fecha de contratación, muestra la diferencia de su salario respecto al empleado anterior.
 
+
+WITH empleados_sueldo AS
+(
+SELECT
+	p.FirstName As Nombre,
+	p.LastName AS Apellido,
+	SUM(eph.rate) OVER (
+	PARTITION BY eph.BusinessEntityID
+	) AS Sueldo,
+	e.HireDate AS fecha_contratacion
+FROM HumanResources.EmployeePayHistory eph
+INNER JOIN Person.Person p
+	ON p.BusinessEntityID=eph.BusinessEntityID
+INNER JOIN HumanResources.EmployeeDepartmentHistory edh
+	ON edh.BusinessEntityID= eph.BusinessEntityID
+INNER JOIN HumanResources.Employee e
+	ON e.BusinessEntityID = eph.BusinessEntityID
+)
+SELECT 
+    Nombre,
+    Apellido,
+    Sueldo,
+    fecha_contratacion,
+    LAG(Sueldo) OVER (
+        ORDER BY fecha_contratacion
+    ) AS Sueldo_Anterior
+FROM empleados_sueldo;
+
+-- aca me quedan repetidos los empleados, tengo que eliminar empleado que hayan cobrado mas de una vez?
+
+WITH sueldo_final AS (
+    SELECT 
+        eph.BusinessEntityID,
+        eph.Rate AS Sueldo,
+        ROW_NUMBER() OVER (
+		PARTITION BY eph.BusinessEntityID 
+		ORDER BY eph.RateChangeDate DESC) AS rn
+    FROM HumanResources.EmployeePayHistory eph
+),
+empleados_unicos AS (
+    SELECT 
+        e.BusinessEntityID,
+        p.FirstName AS Nombre,
+        p.LastName AS Apellido,
+        e.HireDate,
+        s.Sueldo
+    FROM HumanResources.Employee e
+    INNER JOIN Person.Person p ON p.BusinessEntityID = e.BusinessEntityID
+    INNER JOIN sueldo_final s ON s.BusinessEntityID = e.BusinessEntityID
+    WHERE s.rn = 1 -- nos quedamos con el último sueldo
+)
+SELECT 
+    Nombre,
+    Apellido,
+    Sueldo,
+    HireDate AS fecha_contratacion,
+    LAG(Sueldo) OVER (ORDER BY HireDate) AS Sueldo_Anterior
+FROM empleados_unicos;
+
+
 -- 6. Salario máximo y mínimo por departamento.
 -- Muestra el salario actual de cada empleado, el salario máximo y el mínimo dentro de su departamento.
+
+SELECT TOP 5 * FROM HumanResources.EmployeePayHistory
+SELECT TOP 5 * FROM Person.Person
+SELECT TOP 5 * FROM HumanResources.EmployeeDepartmentHistory
+SELECT TOP 5 * FROM HumanResources.Employee
 
 -- 7. Identificar empleados con salario mayor al promedio del departamento.
 -- Lista los empleados cuyo salario (Rate) es mayor al promedio de su departamento usando AVG() OVER (PARTITION BY ...).
 
 
 -- 8. Duración entre órdenes consecutivas.
--- Para cada cliente, calcula cuántos días pasaron entre cada orden y la anterior (LAG() o LEAD()).
+-- Para cada cliente, calcula cuántos días pasaron entre cada orden y la anterior (LAG() o LEAD()).
+
 -- 9. Top 3 productos más vendidos por categoría.
 -- Obtén los tres productos más vendidos (por cantidad) en cada categoría de producto usando DENSE_RANK().
 
 
 -- 10. Detección de cambios de tarifa.
--- Para cada empleado, detecta cuándo cambió su tarifa (Rate) respecto a la anterior usando LAG().
+-- Para cada empleado, detecta cuándo cambió su tarifa (Rate) respecto a la anterior usando LAG().
